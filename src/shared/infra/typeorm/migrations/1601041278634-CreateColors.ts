86c4b5b5161f3b51017bcd0d40af6ee5cebc73b0
Luiz Mauro <luizmetec@hotmail.com>
2020-09-25 13:17:07 -0300

    default class

--- src/shared/infra/typeorm/migrations/1601041278634-CreateColors.ts
+++ src/shared/infra/typeorm/migrations/1601041278634-CreateColors.ts
@@ -1,41 +1,38 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateColors1601041278634 implements MigrationInterface {
+export default class CreateColors1601041278634 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'col_colors',
+        columns: [
+          {
+            name: 'colID',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'colDescription',
+            type: 'varchar',
+          },
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-              name: 'col_colors',
-              columns: [
-                {
-                  name: 'colID',
-                  type: 'varchar',
-                  isPrimary: true,
-                  generationStrategy: 'uuid',
-                },
-                {
-                  name: 'colDescription',
-                  type: 'varchar',
-                },
-                
-                {
-                  name: 'colDateCreated',
-                  type: 'timestamp',
-                  default: 'now()',
-                },
-                {
-                  name: 'colDateUpdated',
-                  type: 'timestamp',
-                  default: 'now()',
-                },
-              ],
-              
-            }),
-        );
-    }
+          {
+            name: 'colDateCreated',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'colDateUpdated',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('col_colors');
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('col_colors');
+  }
 }
