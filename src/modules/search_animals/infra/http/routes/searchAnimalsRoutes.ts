8aedbd85a5ca3d0c369c854443cdb0b7572ea3ea
Luiz Mauro <luizmetec@hotmail.com>
2020-10-04 01:10:14 -0300

    adicionado nova rota /search-animals/range, chamando a nova função finByRange

--- src/modules/search_animals/infra/http/routes/searchAnimalsRoutes.ts
+++ src/modules/search_animals/infra/http/routes/searchAnimalsRoutes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import authenticated from '@modules/users/infra/http/middlewares/Authenticated';
 import SearchAnimalController from '../controllers/SearchAnimalController';
+import GetRangeSearchAnimalsController from '../controllers/GetRangeSearchAnimalsController';
 
 const searchAnimalRouter = Router();
 const searchAnimalController = new SearchAnimalController();
+const getRangeSearchAnimalsController = new GetRangeSearchAnimalsController();
 
 searchAnimalRouter.use(authenticated);
 
 searchAnimalRouter.post('/', searchAnimalController.create);
 searchAnimalRouter.get('/', searchAnimalController.index);
+searchAnimalRouter.get('/range', getRangeSearchAnimalsController.index);
 export default searchAnimalRouter;
