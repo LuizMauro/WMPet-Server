1302f9f198cdb9593cfadbe13a68b90a1a9fb92a
Nilodam <nickricardo@hotmail.com>
2020-12-07 23:15:09 -0300

    remove color and furs authentication

--- src/modules/colors/infra/http/routes/colorsRoutes.ts
+++ src/modules/colors/infra/http/routes/colorsRoutes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 
-import authenticated from '@modules/users/infra/http/middlewares/Authenticated';
 import ColorsController from '../controllers/ColorsController';
 
 const colorsRouter = Router();
 const colorsController = new ColorsController();
 
-colorsRouter.use(authenticated);
 
 colorsRouter.post('/', colorsController.create);
 colorsRouter.get('/', colorsController.index);
--- src/modules/furs/infra/http/routes/fursRoutes.ts
+++ src/modules/furs/infra/http/routes/fursRoutes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 
-import authenticated from '@modules/users/infra/http/middlewares/Authenticated';
 import FursController from '../controllers/FursController';
 
 const fursRouter = Router();
 const fursController = new FursController();
 
-fursRouter.use(authenticated);
 
 fursRouter.post('/', fursController.create);
 fursRouter.get('/', fursController.index);
